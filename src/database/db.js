e698f37d1ae2e91fef26a692ed52545c45c7dbf8
Angelo Carvalho <angelosuporteti@gmail.com>
2020-08-09 14:19:13 -0300

    Corrigindo erros na sintaxe das tabelas que impediam o db abrir

--- src/database/db.js
+++ src/database/db.js
@@ -3,7 +3,7 @@ Database.open(__dirname + '/database.sqlite').then(execute)
 
 function execute(db) {
     db.exec(`
-        CREATE TABLE IF NOT EXITIS proffys (
+        CREATE TABLE IF NOT EXISTS proffys (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT,
             avatar TEXT,
@@ -12,19 +12,18 @@ function execute(db) {
         );
 
         CREATE TABLE IF NOT EXISTS classes (
-            id INTERGER PRIMARY KEY AUTOINCREMENT,
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
             subject TEXT,
             cost TXT,
             proffy_id INTEGER
         );
 
-        CREATE TABLE IF NOT EXISTIS class_schedule (
+        CREATE TABLE IF NOT EXISTS class_schedule (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             class_id INTEGER,
             weekday INTEGER,
             time_from INTEGER,
             time_to INTEGER
         );
-
     `)
 }
